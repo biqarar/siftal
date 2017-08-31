53a9274722bbb4db0d08d0b36072a94e45854382
Javad Evazzadeh <J.Evazzadeh@live.com>
2017-08-31 09:10:06 +0430

    change folder of styles from dist to css

--- src/css/runSass.bat
+++ src/css/runSass.bat
@@ -1,4 +1,4 @@
-sass --watch source.scss:../../dist/siftal.css --sourcemap
+sass --watch source.scss:../../css/siftal.css --sourcemap
 
 #sass --watch source.scss:../siftal.css --style compressed --sourcemap
 
