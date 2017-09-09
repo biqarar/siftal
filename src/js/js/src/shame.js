b48ffae6d0256560caba3a9208f603929391b7b0
Javad Evazzadeh <J.Evazzadeh@live.com>
2017-09-09 12:29:18 +0430

    some fix on file preview

--- src/js/js/src/shame.js
+++ src/js/js/src/shame.js
@@ -80,56 +80,127 @@ function setLanguageURL()
  */
 function showFilePreview()
 {
-  $('input[type="file"][data-preview]').on('change', function()
+  $('input[type="file"]').on('change', function()
   {
-    var myLabel   = $(this).parent().find('label');
-    var myImgPrev = myLabel.find('img');
-    var myImgSrc  = null;
-
-    if(myImgPrev.length < 1)
+    var $this    = $(this);
+    var $fileBox = $this.parents('.input');
+    var myFile   = this.files[0];
+    // user not select file, cancel choosing file
+    if(!myFile)
     {
-      // create img element and replace it in html
-      var myImg = document.createElement("img");
-      myImg.id  = $(this).attr('id') + 'Preview';
-      myLabel.html(myImg);
-      // get new inserted img
-      myImgPrev = myLabel.find('img');
+      $this.attr('data-size', null);
+      return false;
     }
+    // get selected file size
+    var fileSize = Math.round( myFile.size / 1024);
+    $this.attr('data-size', fileSize);
 
-    // get image path and ext
-    var imgPath = $(this)[0].value;
-    var imgExt  = imgPath.substring(imgPath.lastIndexOf('.') + 1).toLowerCase();
-    // if selected valid extention for preview
-    if (imgExt == "gif" || imgExt == "png" || imgExt == "jpg" || imgExt == "jpeg")
+
+
+    console.log(myFile);
+    console.log(fileSize);
+
+    // check for min file size
+    if($this.attr('data-min'))
     {
-      // if file reader is defined!
-      if (typeof (FileReader) != "undefined")
+      // if size is less than needed size
+      if(fileSize < $this.attr('data-min'))
       {
-        // create new reader
-        var reader = new FileReader();
-
-        reader.onload = function (e)
-        {
-          // get loaded data and render thumbnail.
-          myImgPrev.attr('src', e.target.result);
-        };
-
-        // read the image file as a data URL.
-        reader.readAsDataURL(this.files[0]);
-        myImgSrc = true;
+        $fileBox.addClass('error');
+        $fileBox.removeClass('ok');
       }
       else
       {
-        console.log('file reader is undefined!');
-        var blobURL = window.URL.createObjectURL(this.files[0]);
-        myImgPrev.attr('src', blobURL);
-        myImgSrc = true;
+        $fileBox.addClass('ok');
+        $fileBox.removeClass('error');
       }
     }
-    else
+    // check for max file size
+    if($this.attr('data-max'))
     {
-      console.log('we cant preview this type of file');
-      myImgPrev.attr('src', null);
+      // if size is less than needed size
+      if(fileSize > $this.attr('data-max'))
+      {
+        $fileBox.addClass('error');
+        $fileBox.removeClass('ok');
+      }
+      else
+      {
+        $fileBox.addClass('ok');
+        $fileBox.removeClass('error');
+      }
+    }
+
+
+    // if have preview start creating preview
+    if($this.attr('data-preview') !== undefined)
+    {
+      if(fileSize > 1024)
+      {
+        $fileBox.addClass('error');
+        $fileBox.removeClass('ok');
+        $fileBox.removeClass('warning');
+        return false;
+      }
+      else
+      {
+        $fileBox.removeClass('error');
+      }
+        $fileBox.removeClass('ok');
+        $fileBox.removeClass('warning');
+
+      var myLabel   = $this.parent().find('label');
+      var myImgPrev = myLabel.find('img');
+      var myImgSrc  = null;
+
+      if(myImgPrev.length < 1)
+      {
+        // create img element and replace it in html
+        var myImg = document.createElement("img");
+        myImg.id  = $this.attr('id') + 'Preview';
+        myLabel.html(myImg);
+        // get new inserted img
+        myImgPrev = myLabel.find('img');
+      }
+
+      // get image path and ext
+      var imgPath = $this[0].value;
+      var imgExt  = imgPath.substring(imgPath.lastIndexOf('.') + 1).toLowerCase();
+      // if selected valid extention for preview
+      if (imgExt == "gif" || imgExt == "png" || imgExt == "jpg" || imgExt == "jpeg")
+      {
+        // if file reader is defined!
+        if (typeof (FileReader) != "undefined")
+        {
+          // create new reader
+          var reader = new FileReader();
+
+          reader.onload = function (e)
+          {
+            // get loaded data and render thumbnail.
+            myImgPrev.attr('src', e.target.result);
+          };
+
+          // read the image file as a data URL.
+          reader.readAsDataURL(myFile);
+          myImgSrc = true;
+          $fileBox.addClass('ok');
+        }
+        else
+        {
+          console.log('file reader is undefined!');
+          var blobURL = window.URL.createObjectURL(myFile);
+          myImgPrev.attr('src', blobURL);
+          myImgSrc = true;
+          $fileBox.addClass('warning');
+        }
+      }
+      else
+      {
+        console.log('we cant preview this type of file');
+        myImgPrev.attr('src', null);
+        $fileBox.addClass('warning');
+      }
     }
   });
 }
