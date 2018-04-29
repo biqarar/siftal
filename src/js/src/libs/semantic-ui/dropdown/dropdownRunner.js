93ccad7ccfef081f5b861d63fa73b97592246b41
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-04-29 17:17:46 +0430

    fix read remote data

--- src/js/src/libs/semantic-ui/dropdown/dropdownRunner.js
+++ src/js/src/libs/semantic-ui/dropdown/dropdownRunner.js
@@ -1,7 +1,27 @@
 
-
 function dropdownRunner()
 {
-	$('.ui.dropdown').dropdown();
+  $('.ui.dropdown').each(function ()
+  {
+    // allow add new value
+    if($(this).hasClass('addition'))
+    {
+      $(this).dropdown({allowAdditions: true});
+      console.log($(this));
+      return true;
+    }
+
+    // run with remote source
+    if($(this).attr('data-source') !== undefined)
+    {
+      $(this).dropdown(
+      {
+        apiSettings: {url: $(this).attr('data-source')}
+      });
+      return true;
+    }
+    // run normal dropdown
+    $(this).dropdown();
+  });
 }
 
