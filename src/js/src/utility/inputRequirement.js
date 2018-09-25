b2092e1062c4f23a9f2d9d9a0aea6e35ca52f0d3
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-09-25 20:44:46 +0330

    only highlight empty requiremnets

--- src/js/src/utility/inputRequirement.js
+++ src/js/src/utility/inputRequirement.js
@@ -5,7 +5,8 @@ function inputRequirement()
   $(document).on('mouseenter mouseleave', 'form .btn', function()
   {
     myForm      = $(this).parents('form');
-    myEmptyVals = myForm.find('input[required]:empty');
+    myEmptyVals = myForm.find('input[required]').filter(function() { return !$(this).val().trim().length })
+
     myEmptyVals.addClass('requirement');
 
   // remove requirement class after delay
