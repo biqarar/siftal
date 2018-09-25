82a432730c5aae449d89370f747ee0c9225b70d1
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-09-25 20:39:10 +0330

    chnage delay of requirement to 1sec

--- src/js/src/utility/inputRequirement.js
+++ src/js/src/utility/inputRequirement.js
@@ -4,14 +4,14 @@ function inputRequirement()
 {
   $(document).on('mouseenter mouseleave', 'form .btn', function()
   {
-	myForm      = $(this).parents('form');
-	myEmptyVals = myForm.find('input[required]:empty');
-  	myEmptyVals.addClass('requirement');
+    myForm      = $(this).parents('form');
+    myEmptyVals = myForm.find('input[required]:empty');
+    myEmptyVals.addClass('requirement');
 
-	// remove requirement class after delay
-  	setTimeout(function() {
-  		myEmptyVals.removeClass('requirement');
-  	}, 3000);
+  // remove requirement class after delay
+    setTimeout(function() {
+      myEmptyVals.removeClass('requirement');
+  	}, 1000);
   });
 
 };
