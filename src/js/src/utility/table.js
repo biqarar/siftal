4edb9b5240d15bca9bbbb8a374b4990598f27577
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-11-19 15:55:12 +0330

    on hoverable table skip click on links

--- src/js/src/utility/table.js
+++ src/js/src/utility/table.js
@@ -2,9 +2,16 @@
 
 function tbl1Openable()
 {
-  $(document).on('click', '.tbl1.openable tbody tr', function(e)
+  $(document).on('click', '.tbl1.openable tbody tr', function(_e)
   {
-    $(this).toggleClass('open');
+  	if($(_e.target).is('a, a *'))
+  	{
+  		// on click on links skip
+  	}
+  	else
+  	{
+    	$(this).toggleClass('open');
+  	}
   });
 }
 
