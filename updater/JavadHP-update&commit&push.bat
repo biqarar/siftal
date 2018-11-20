4a1f2610e222d1e053a0bbc6b675e127846eae8b
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-11-20 13:24:59 +0330

    add sample of auto commit and push

---
+++ updater/JavadHP-update&commit&push.bat
@@ -0,0 +1,91 @@
+SET SIFTAL=%~dp0%..\dist
+SET PROJECTS=%~dp0%..\..\
+
+
+xcopy "%SIFTAL%" "%PROJECTS%\azvir\public_html\static\siftal" /E /H /K /Y
+cd "%PROJECTS%\azvir\"
+git add .
+git commit -m "Automatic update Siftal!"
+git push origin master
+
+
+xcopy "%SIFTAL%" "%PROJECTS%\dashSample\public_html\static\siftal" /E /H /K /Y
+cd "%PROJECTS%\dashSample\"
+git add .
+git commit -m "Automatic update Siftal!"
+git push origin master
+
+
+
+xcopy "%SIFTAL%" "%PROJECTS%\deadbrowser\public_html\static\siftal" /E /H /K /Y
+cd "%PROJECTS%\deadbrowser\"
+git add .
+git commit -m "Automatic update Siftal!"
+git push origin master
+
+
+
+xcopy "%SIFTAL%" "%PROJECTS%\ermile\public_html\static\siftal" /E /H /K /Y
+cd "%PROJECTS%\ermile\"
+git add .
+git commit -m "Automatic update Siftal!"
+git push origin master
+
+
+
+xcopy "%SIFTAL%" "%PROJECTS%\iranpresidents\public_html\static\siftal" /E /H /K /Y
+cd "%PROJECTS%\iranpresidents\"
+git add .
+git commit -m "Automatic update Siftal!"
+git push origin master
+
+
+
+xcopy "%SIFTAL%" "%PROJECTS%\jibres\public_html\static\siftal" /E /H /K /Y
+cd "%PROJECTS%\jibres\"
+git add .
+git commit -m "Automatic update Siftal!"
+git push origin master
+
+
+
+xcopy "%SIFTAL%" "%PROJECTS%\khadije\public_html\static\siftal" /E /H /K /Y
+cd "%PROJECTS%\khadije\"
+git add .
+git commit -m "Automatic update Siftal!"
+git push origin master
+
+
+
+xcopy "%SIFTAL%" "%PROJECTS%\sarshomar\public_html\static\siftal" /E /H /K /Y
+cd "%PROJECTS%\sarshomar\"
+git add .
+git commit -m "Automatic update Siftal!"
+git push origin master
+
+
+
+xcopy "%SIFTAL%" "%PROJECTS%\taraztax\public_html\static\siftal" /E /H /K /Y
+cd "%PROJECTS%\taraztax\"
+git add .
+git commit -m "Automatic update Siftal!"
+git push origin master
+
+
+
+xcopy "%SIFTAL%" "%PROJECTS%\tejarak\public_html\static\siftal" /E /H /K /Y
+cd "%PROJECTS%\tejarak\"
+git add .
+git commit -m "Automatic update Siftal!"
+git push origin master
+
+
+
+xcopy "%SIFTAL%" "%PROJECTS%\karimehDonate\public_html\static\siftal" /E /H /K /Y
+cd "%PROJECTS%\karimehDonate\"
+git add .
+git commit -m "Automatic update Siftal!"
+git push origin master
+
+
+
