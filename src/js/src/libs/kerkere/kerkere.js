05005bee7fd29f31543dd6a275e4a558efd52a61
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-11-28 21:40:05 +0330

    add icon

--- src/js/src/libs/kerkere/kerkere.js
+++ src/js/src/libs/kerkere/kerkere.js
@@ -9,15 +9,32 @@ function kerkere()
 
     if(myTarget.length)
     {
+      // if want icon
+      if($this.attr('data-kerkere-icon') !== undefined)
+      {
+        if(myTarget.is(":visible"))
+        {
+          // on next this will closed
+          $this.attr('data-kerkere-icon', 'close');
+        }
+        else
+        {
+          // on next this will open
+          $this.attr('data-kerkere-icon', 'open');
+        }
+      }
+
       //Expand or collapse this panel
       myTarget.slideToggle('fast');
     }
 
-    //Hide the other panels
     if($this.attr('data-kerkere-single') !== undefined)
     {
+
+      //Hide the other panels
       $('[data-kerkere-content]').not(myTarget).slideUp('fast');
-      // $(".kerkereContent").not($(this).next()).slideUp('fast');
+      // change icon of all other
+      $('[data-kerkere]').not($this).attr('data-kerkere-icon', 'close');
     }
   });
 }
