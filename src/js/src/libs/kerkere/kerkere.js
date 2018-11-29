e73789dfafa027c618471160cec0a771c3f1235f
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-11-29 21:07:11 +0330

    allow to share status of kerkere to another element

--- src/js/src/libs/kerkere/kerkere.js
+++ src/js/src/libs/kerkere/kerkere.js
@@ -12,42 +12,48 @@ function kerkereRunner()
 function kerkere(_this)
 {
     var myTarget = null;
+    var $target  = null;
     if(_this)
     {
       myTarget = _this.attr('data-kerkere');
       if(myTarget)
       {
-        myTarget = $(myTarget);
+        $target = $(myTarget);
       }
     }
 
-    if(myTarget && myTarget.length)
+    if($target && $target.length)
     {
+      // show status if needed
+      var myStatusEl = '[data-kerkere-look="'+ myTarget +'"]';
+
       // if want icon
       if(_this.attr('data-kerkere-icon') !== undefined)
       {
-        if(myTarget.is(":visible"))
+        if($target.is(":visible"))
         {
           // on next this will closed
           _this.attr('data-kerkere-icon', 'close');
+          $(myStatusEl).attr('data-kerkere-status', 'close');
         }
         else
         {
           // on next this will open
           _this.attr('data-kerkere-icon', 'open');
+          $(myStatusEl).attr('data-kerkere-status', 'open');
         }
       }
 
       //Expand or collapse this panel
-      myTarget.slideToggle('fast');
+      $target.slideToggle('fast');
     }
 
     if(_this.attr('data-kerkere-single') !== undefined)
     {
-
       //Hide the other panels
-      $('[data-kerkere-content]').not(myTarget).slideUp('fast');
+      $('[data-kerkere-content]').not($target).slideUp('fast');
       // change icon of all other
       $('[data-kerkere][data-kerkere-icon]').not(_this).attr('data-kerkere-icon', 'close');
     }
 }
+
