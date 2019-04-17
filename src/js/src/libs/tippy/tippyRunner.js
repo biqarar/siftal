cdd0ac46bac260f547f9f350423d79c1689fbf5c
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-04-17 11:46:51 +0430

    update tippy to v4.2.1

--- src/js/src/libs/tippy/tippyRunner.js
+++ src/js/src/libs/tippy/tippyRunner.js
@@ -2,34 +2,40 @@ var myTippy;
 
 function runTippy()
 {
-	if($('html').attr('data-app') === undefined)
-	{
-		// removeTippy();
-		myTippy = tippy('[title]',
-		{
-			arrow: true,
-			animation: 'scale',
-			dynamicTitle: true,
-		});
-	}
+  if($('html').attr('data-app') === undefined)
+  {
+    // removeTippy();
+    myTippy = tippy('[title]',
+    {
+      arrow: true,
+      animation: 'scale',
+      content(reference)
+      {
+        const title = reference.getAttribute('title')
+        reference.removeAttribute('title')
+        return title
+      },
+      // dynamicTitle: true,
+    });
+  }
 }
 
 
 function removeTippy()
 {
-	var allTippy = document.querySelectorAll('[data-tippy]');
+  var allTippy = document.querySelectorAll('[data-tippy]');
 
-	allTippy.forEach(function(_el)
-	{
-		if(_el._tippy)
-		{
-			_el._tippy.hide();
-		}
-	});
+  allTippy.forEach(function(_el)
+  {
+    if(_el._tippy)
+    {
+      _el._tippy.hide();
+    }
+  });
 
-	// if(myTippy)
-	// {
-	// 	myTippy.destroyAll();
-	// }
+  // if(myTippy)
+  // {
+  //  myTippy.destroy();
+  // }
 }
 
