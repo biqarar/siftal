125bb2f2643367c63d7ebf8c81786aa383a04fb5
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-05-10 09:55:16 +0430

    need more test

--- src/js/src/utility/serviceWorker.js
+++ src/js/src/utility/serviceWorker.js
@@ -22,21 +22,4 @@ function registerServiceWorker()
 	{
 		console.log('service worker is not supported');
 	}
-
-
-	window.addEventListener('beforeinstallprompt', e =>
-	{
-		console.log('beforeinstallprompt Event fired');
-		e.preventDefault();
-		// Stash the event so it can be triggered later.
-		this.deferredPrompt = e;
-		return false;
-	});
-	// When you want to trigger prompt:
-	this.deferredPrompt.prompt();
-	this.deferredPrompt.userChoice.then(choice =>
-	{
-		console.log(choice);
-	});
-	this.deferredPrompt = null;
 }
