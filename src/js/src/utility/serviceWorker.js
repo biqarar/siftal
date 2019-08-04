6386f0f8370b6382aca5d53ebd5562e46fac2072
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-08-04 19:21:10 +0430

    temporary disable service worker

--- src/js/src/utility/serviceWorker.js
+++ src/js/src/utility/serviceWorker.js
@@ -1,17 +1,20 @@
 function registerServiceWorker()
 {
+	removeAllServiceWorkers();
+	return false;
+	// need more work
 	if ('serviceWorker' in navigator)
 	{
 		window.addEventListener('load', () =>
 		{
-			navigator.serviceWorker.register('/serviceWorker/v2').then(function(_reg)
+			navigator.serviceWorker.register('/serviceWorker-v3?v=3').then(function(_reg)
 			{
 				// Registration was successful
-				logy('Service worker registered.', _reg);
+				logy('Service worker registered.');
 			}, function(_err)
 			{
 				// registration failed :(
-				logy('ServiceWorker registration failed: ', _err);
+				logy('ServiceWorker registration failed.');
 			}).catch(function(_err)
 			{
 				logy(_err);
@@ -21,5 +24,22 @@ function registerServiceWorker()
 	else
 	{
 		logy('service worker is not supported');
+	}
+}
+
+function removeAllServiceWorkers()
+{
+	if ('serviceWorker' in navigator) {
+	  navigator.serviceWorker.getRegistrations().then(function (registrations)
+	  {
+	    //returns installed service workers
+	    if (registrations.length)
+	    {
+	      for(let registration of registrations)
+	      {
+	        registration.unregister();
+	      }
+	    }
+	  });
 	}
 }
